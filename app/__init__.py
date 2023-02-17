5629120c6852b5cc787f5e22932290b2492712e7
ADIVADER19 <advaitnurani@gmail.com>
2023-02-17 16:50:37 +0530

    auth added and login func fixes

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,16 @@
-from flask import Flask
 from flask_cors import CORS
 from flask_pymongo import pymongo
+from flask import Flask, render_template, redirect, request, session
+from flask_session import Session
+import redis
+
 app=Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "redis"
+app.config['SESSION_USE_SIGNER'] = True
+app.config['SESSION_REDIS'] = redis.from_url('redis://127.0.0.1:6379')
 app.config['SECRET_KEY'] = 'eb2879129ee008af7397c67e06a18f636560786d'
+Session(app)
 conn_string = "mongodb+srv://hridayesh:WN7WWkbBNk7viXB2@cluster0.hlqlkgj.mongodb.net/?retryWrites=true&w=majority"
 mongoDB_client = pymongo.MongoClient(conn_string)
 db = mongoDB_client.get_database('summarizer')
--- app/routes.py
+++ app/routes.py
@@ -25,3 +25,11 @@ def login():
     except:
         print("Error")
         
+        
+@app.route('/auth', methods=['GET', 'POST'])
+def auth():
+    try:
+         return controllers.AudioController.auth()
+    except:
+        print("Error")
+        
