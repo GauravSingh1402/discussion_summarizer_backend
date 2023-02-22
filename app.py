3183e7e2c0f4535354414afb6ce4256f04cb46ee
ADIVADER19 <advaitnurani@gmail.com>
2023-02-22 11:01:34 +0530

    changes in app.py

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ import os
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     # Start the Flask application on the correct port
-    app.run(host="0.0.0.0", port=port)
+    app.run(host='0.0.0.0', port=port)
