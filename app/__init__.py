e392c42f5baaa63362cf0e929a042639e72cbc76
ADIVADER19 <advaitnurani@gmail.com>
2023-02-25 20:12:13 +0530

    changed init file

--- app/__init__.py
+++ app/__init__.py
@@ -20,5 +20,5 @@ Session(app)
 conn_string = config.Config.DATABASE_URI
 mongoDB_client = pymongo.MongoClient(conn_string)
 db = mongoDB_client.get_database('summarizer')
-CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app, resources={r"/*": {"origins": "*"}},supports_credentials=True)
 from app import routes
